5fb1d25ec59283215cc6be0ec4d2459040d4c45b
haneulchoistudio <haneulchoi.business@gmail.com>
2024-04-23 04:09:26 +0000

    added html, script to serve html

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const http = require("http");
+const fs = require("fs");
 const PORT = 3000;
 
 http.createServer(function (req, res) {
-	res.write('Digital Ocean Created NodeJS Server');
-	res.end();
+	fs.readFile('index.html', function (err, data) {
+		res.writeHeader(200, {'content-type': 'text/html'});
+		res.write(html)
+		res.end();
+	})f
 }).listen(PORT);
 
 console.log(`Server Started on Port ${PORT}`);
